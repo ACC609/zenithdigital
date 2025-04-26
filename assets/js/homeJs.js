461f204d58fa84f3fc66278d871ace713702440f
ACC609 <adolfojunior544@gmail.com>
2025-04-26 12:52:47 +0100

    first commit

---
+++ assets/js/homeJs.js
@@ -0,0 +1,9 @@
+const menuOpen = document.getElementById('menu')
+
+function open_menu(){
+    menuOpen.classList.add('active')
+}
+
+function close_menu(){
+    menuOpen.classList.remove('active')
+}
